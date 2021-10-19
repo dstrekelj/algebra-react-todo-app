7c38f77b472477a113515b04a8e2546f11b12022
Domagoj Štrekelj <domagojstrekelj@gmail.com>
2021-10-19 20:34:16 +0200

    Add todo item form

--- src/App.js
+++ src/App.js
@@ -1,37 +1,11 @@
-import { useState } from "react";
-import { useDispatch } from "react-redux";
-import { addItem } from "./reducers/ToDoReducer";
 import ToDoItemList from "./components/ToDoItemList";
+import ToDoItemForm from "./components/ToDoItemForm";
 
 function App() {
-  const dispatch = useDispatch();
-
-  const [ formState, setFormState ] = useState({ text: "" });
-
-  const handleOnChange = (event) => {
-    event.preventDefault();
-    setFormState((formState) => ({
-      ...formState,
-      [event.target.name]: event.target.value,
-    }));
-  }
-
-  const handleOnSubmit = (event) => {
-    event.preventDefault();
-    dispatch(addItem(formState));
-    setFormState({ text: "" });
-  }
-
   return (
     <div>
       <ToDoItemList />
-      <form onSubmit={handleOnSubmit}>
-        <input name="text" type="text" placeholder="Item text"
-          onChange={handleOnChange}
-          value={formState.text}
-        />
-        <button type="submit">Add Item</button>
-      </form>
+      <ToDoItemForm />
     </div>
   );
 }
